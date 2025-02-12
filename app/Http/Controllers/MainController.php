8872c5c55104368bd0730ca0c7d334f869eda276
ageu_dev19 <ageumenezes423@gmail.com>
2025-02-12 08:43:00 -0300

    Start preparing the game

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
@@ -13,8 +14,28 @@ class MainController extends Controller
         $this->app_data = require(app_path('app_data.php'));
     }
 
-    public function showData()
+    public function startGame(): View
     {
-        return response()->json($this->app_data);
+        return view('home', ['app_data' => $this->app_data]);
+    }
+
+    public function prepareGame(Request $request)
+    {
+        $request->validate([
+            'total_questions' => 'required|integer|min:3|max:30',
+        ],
+        [
+            'total_questions.required' => 'Please enter the total number of questions.',
+            'total_questions.integer' => 'The total number of questions must be an integer.',
+            'total_questions.min' => 'The total number of questions must be at least 3.',
+            'total_questions.max' => 'The total number of questions must not be greater than 30.',
+        ]
+    );
+
+        $total_questions = intval($request->input('total_questions'));
+
+        // $quiz = $this->prepareQuiz($total_questions);
+
+        // dd($quiz);
     }
 }
